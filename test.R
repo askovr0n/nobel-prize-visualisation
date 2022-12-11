3611219c9482c8f95a2e2a3de4142bf7fa1762fb
askovr0n <artur.skowronski6@gmail.com>
2022-12-11 13:42:22 +0100

    Plots_based_on_exercises

---
+++ test.R
@@ -0,0 +1,140 @@
+library(ggplot2)
+library(dplyr)
+library(scales)
+library(RColorBrewer)
+library(lubridate)
+library(stringr)
+library(readr)
+
+nobel <- read_csv('complete.csv')
+
+# Preprocessing
+
+nobel <- mutate(nobel, birth_date = ifelse(birth_date == "1943-00-00", "1943-11-07", birth_date))
+
+nobel$laureate_or_org = if_else(is.na(nobel$knownName), "Organization", "Individual")
+nobel$laureate_or_org = factor(nobel$laureate_or_org, levels = c("Individual", "Organization"))
+
+nobel$category = factor(nobel$category, levels = c('Physics', 'Chemistry', 'Physiology or Medicine',
+                                                             'Peace', 'Literature', 'Economic Sciences'))
+
+nobel$prizeStatus <- str_to_title(nobel$prizeStatus)
+nobel$prizeStatus <- factor(nobel$prizeStatus, levels = c('Received', 'Declined', 'Restricted'))
+
+nobel$gender <- str_to_title(nobel$gender)
+nobel$gender <- factor(nobel$gender, levels = c('Male', 'Female'))
+
+nobel$gen_org <- if_else(nobel$gender == 'Male', "Male", "Female", "Organization")
+nobel$gen_org <- factor(nobel$gen_org, levels = c("Male", "Female", "Organization"))
+
+nobel <- nobel %>%
+  mutate(awardDate = as.Date(ISOdate(awardYear, 10, 01)), # approximating 
+         age_days = ifelse(birth_date == "", "unnknown", awardDate - as.Date(birth_date)),
+         age_years = round(interval(as.Date(birth_date), awardDate) / years(1)), 
+         life_span_days = as.Date(death_date) - as.Date(birth_date), 
+         life_span_years = round(interval(as.Date(birth_date), death_date) / years(1)), 
+         is_alive = laureate_or_org == 'Laureate' & is.na(death_date))
+
+ # Visualisations - from lectures
+
+# Plot 1
+
+nobel$awardYear_cut <- cut(nobel$awardYear, 10, dig.lab=4)
+
+nobel_no_year_gender <- nobel %>% 
+  group_by(awardYear_cut, gender) %>% 
+  summarise(number = length(awardYear_cut))
+
+ggplot(data = nobel_no_year_gender, aes(x = awardYear_cut, y = number, fill = gender)) +
+  geom_bar(stat = 'identity', color = 'darkgreen') +
+  theme_minimal() 
+
+# solution - adjust the geom_label:
+ggplot(data = nobel_no_year_gender, aes(x = awardYear_cut, y = number, fill = factor(gender))) +
+  geom_bar(na.rm = TRUE, position = "stack", width = 0.5, stat = "identity") +
+  ggtitle('Gender and age in the sample') +
+  xlab('Age') +
+  ylab('Population') +
+  labs(fill = 'Gender') +
+  theme_minimal() +
+  scale_fill_manual(values = c('grey78', 'khaki', 'blue')) +
+  geom_label(aes(y = number, label = number), fill = 'white', position = "identity",
+             vjust = 0.5, color = "black", size = 3) +
+  theme(axis.text.x = element_text(angle=45, hjust=1))
+
+# Plot 2 - let's look into the last decade more closely. Adjust geom_label (again)
+
+last_decade_no <- nobel %>% 
+  group_by(awardYear, gender) %>% 
+  summarise(number = length(awardYear))
+
+last_decade_no_adj <- last_decade_no %>% 
+  arrange(awardYear, desc(gender)) %>% 
+  group_by(awardYear) %>% 
+  mutate(label_sum = cumsum(number))
+
+final <- last_decade_no_adj %>% 
+  mutate(label_sum2 = label_sum*.5)
+
+ggplot(data = final, aes(x = factor(awardYear), y = number, fill = factor(gender))) +
+  geom_bar(na.rm = TRUE, position = "stack", width = 0.7, stat = "identity") +
+  ggtitle('Gender and age in the sample') +
+  xlab('Age') +
+  ylab('Population') +
+  labs(fill = 'Gender') +
+  theme_minimal() +
+  geom_label(aes(y = label_sum2, label = number), fill = 'white', position = position_stack(),
+             vjust = 0.5, color = "black", size = 3) +
+  scale_x_discrete(limits = as.character(seq(2008, 2019, 1)))
+
+# Plot 3 - raczej useless
+
+mean_no_of_nobels_men <- mean(final[final$gender == "Male", ]$label_sum, na.rm=T)
+mean_no_of_nobels_women <- mean(final[final$gender == "Female", ]$label_sum, na.rm=T)
+
+ggplot(data = final, aes(x = awardYear, y = label_sum, group = factor(gender))) +
+  geom_line(aes(linetype = factor(gender), color = factor(gender)), size = 2) +
+  geom_point(size = 3, aes(shape = factor(gender))) +
+  theme_minimal() +
+  geom_line(aes(y=mean_no_of_nobels_men), color = "blue") +
+  geom_line(aes(y=mean_no_of_nobels_women), color = "red") +
+  ylab("Number of Nobel Prizes") +
+  xlab("Year")
+
+# Plot 4 - usage of ggplot extensions
+library(ggrepel)
+
+unique_categories <- unique(nobel$awardYear_cut)
+
+nobel_no_year_category <- nobel %>% 
+  group_by(awardYear_cut, category) %>% 
+  summarise(number = length(awardYear_cut)) %>% 
+  mutate(label = if_else(awardYear_cut == levels(nobel$awardYear_cut)[length(unique_categories <- unique(nobel$awardYear_cut))],
+                         as.character(category),
+                         NA_character_))
+
+nobel_no_year_category %>%
+  ggplot(aes(x = awardYear_cut, y = number, group = category, colour = category)) + 
+  geom_line() +
+  theme(axis.text.x = element_text(angle=45, hjust=1)) +
+  geom_label_repel(aes(label = label,
+                 na.rm = TRUE)) +
+  xlab("Decades") +
+  ylab("Number of Nobel Prizes") +
+  ggtitle("Nobel prizes based on categories")
+
+# Plot 5 - table
+
+nobel.agg <- nobel %>%
+  mutate(age_years_cut = cut(age_years, 4)) %>% 
+  count(age_years_cut, category)
+
+ggplot(nobel.agg, aes(x = age_years_cut, y = category)) + # nowa baza danych
+  geom_tile(aes(fill = n), color = 'black', show.legend = F) +
+  theme_minimal() + 
+  geom_text(aes(label = n), size = 5, fontface = 'bold', color = 'white') +
+  labs(title = 'Dispersion of Nobel Prizes',
+       x = "Age",
+       y = "Category") +
+  scale_fill_gradient(low = 'green3', high = 'orange')
+  
